7d3af98ae34986eeefa6edaa4cfed5f19db95b8c
Alexandr Gubankoff <al_1001@ukr.net>
2022-08-24 15:01:33 +0300

    Fix

--- application.py
+++ application.py
@@ -1,9 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+# ------------------------------------------------
+# Program by Denis Astahov
+#
+#
+# Version      Date           Info
+# 1.0          13-Dec-2019    Initial Version
+#
+# ----------------------------------------------
+from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+application = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run()
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+
+@application.route("/hello")
+def index():
+    return "Hello World from Flask Hello Page.<b> v1.0"
+
+#--------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+#------------------------------
